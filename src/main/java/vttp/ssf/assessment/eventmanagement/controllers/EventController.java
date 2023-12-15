3106edb4d05f39fc1d9753dc62702f61311d7056
Megan <megan.koh96@gmail.com>
2023-12-15 15:14:36 +0800

    Initial Commit

--- src/main/java/vttp/ssf/assessment/eventmanagement/controllers/EventController.java
+++ src/main/java/vttp/ssf/assessment/eventmanagement/controllers/EventController.java
@@ -1,22 +1,12 @@
 package vttp.ssf.assessment.eventmanagement.controllers;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpSession;
 import vttp.ssf.assessment.eventmanagement.models.Event;
 import vttp.ssf.assessment.eventmanagement.repositories.RedisRepository;
 
@@ -37,12 +27,7 @@ public class EventController {
 		}
 		return mav;
 	}
-
-	@GetMapping("/events/register/2")
-	public String showPage(Model model){
-		model.addAttribute("event", new Event());
-		return "view1";
-	}
+	
 }
 
 //copy
--- src/main/java/vttp/ssf/assessment/eventmanagement/controllers/RegistrationController.java
+++ src/main/java/vttp/ssf/assessment/eventmanagement/controllers/RegistrationController.java
@@ -1,10 +1,39 @@
 package vttp.ssf.assessment.eventmanagement.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vttp.ssf.assessment.eventmanagement.models.Event;
+import vttp.ssf.assessment.eventmanagement.models.User;
+import vttp.ssf.assessment.eventmanagement.repositories.RedisRepository;
+
+@Controller
+@RequestMapping
 public class RegistrationController {
-    
 
-    // TODO: Task 6
+    @Autowired
+    RedisRepository repo;
 
+    @GetMapping("events/register/{index}")
+    public String register(@PathVariable("index") int index, Model model) {
 
-    // TODO: Task 7
+        Event event = repo.getEvent(index);
+        String eventName = event.getEventName();
+        long eventDate = event.getEventDate();
+        User user = new User();
+        model.addAttribute("eventName", eventName);
+        model.addAttribute("eventDate", eventDate);
+        model.addAttribute("user", user);
+
+        return "eventregistration";
+
+    }
 }
+
+// TODO: Task 7
